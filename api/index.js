b4bdcf085a78d2ec94e3d6bc97d7011abebb021c
Adetola Bewaji <adetola013@gmail.com>
2021-10-01 10:53:26 +0100

    express && other dependencies added && server spin up successfully at the basic level

---
+++ api/index.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var express = require('express');
+const cors = require('cors')
+var bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app = express()
+app.server = http.createServer(app);
+
+app.use(cors()); 
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(async (req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    // next(createError.NotFound())
+    next(error)
+  })
+
+  app.use((err, req, res, next)=>{
+    res.status(err.status || 500)
+    res.json({
+      status: err.status || 500,
+      message: err.message,
+    })
+  })
+
+  var server = app.listen(port, function () { 
+    var host = server.address().address
+    var port = server.address().port
+    console.log("App listening at http://%s:%s", host, port) // 10.0.2.2
+  })
+
+// app.listen(port, () => {
+//   console.log(`istening at http://localhost:${port}`)
+// })
