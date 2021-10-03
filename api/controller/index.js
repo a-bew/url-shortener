42e8435223600806dcf38b97d03138ad9bcc8454
Adetola Bewaji <adetola013@gmail.com>
2021-10-03 13:51:25 +0100

    controller file added

---
+++ api/controller/index.js
@@ -0,0 +1,20 @@
+
+export const encode = async (req, res) => {
+
+}
+
+export const decode = async (req, res) => {
+
+}
+
+export const statistic = async (req, res) => {
+
+}
+
+export const list = async (req, res) => {
+
+}
+
+export const redirectToLong = async (req, res) => {
+
+}
