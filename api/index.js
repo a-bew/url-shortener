b05ab59c4150d3c57aa5541dd69e1cf7049e5977
Adetola Bewaji <adetola013@gmail.com>
2021-10-04 11:16:40 +0100

    fixed endpoints path by removing /api

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/api", endpoints);
+app.use("/", endpoints);
 
 
 const port = 8000
