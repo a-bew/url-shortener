58da359a3f9daed7cb9e0b796b0e35edcda37c5a
Adetola Bewaji <adetola013@gmail.com>
2021-10-05 10:30:54 +0100

    Shortener class updated

--- api/utils.js
+++ api/utils.js
@@ -143,19 +143,4 @@ class Shortener{
 
 }
 
-function isValidHttpUrl(string) {
-
-    let url;
-    
-    try {
-      url = new URL(string);
-    } catch (_) {
-      return false;  
-    }
-  
-    return url.protocol === "http:" || url.protocol === "https:";
-
-}
-
-
 export default Shortener;
