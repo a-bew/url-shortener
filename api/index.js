6a5ef7df4c0fdc8000e2fa429963060cb7edef3c
Adetola Bewaji <adetola013@gmail.com>
2021-10-06 15:06:32 +0100

    console.log removed

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import http from 'http'
 import express from 'express'
+import http from 'http'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import logger from 'morgan'
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-const app = express()
+export const app = express()
 app.server = http.createServer(app);
 
 app.use(cors()); 
@@ -20,8 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", endpoints);
 
-
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -31,20 +29,23 @@ app.use(async (req, res, next) => {
     error.status = 404;
     // next(createError.NotFound())
     next(error)
-  })
+})
 
 const port  =  process.env.PORT;
 
-  app.use((err, req, res, next)=>{
-    res.status(err.status || 500)
-    res.json({
-      status: err.status || 500,
-      message: err.message,
-    })
+app.use((err, req, res, next)=>{
+  res.status(err.status || 500)
+  res.json({
+    status: err.status || 500,
+    message: err.message,
   })
+})
+
+console.log("nodemon nodemon")
 
   var server = app.listen(port, function () { 
     var host = server.address().address
     var port = server.address().port
-    console.log("App listening at http://%s:%s", host, port) // 10.0.2.2
+    console.log("App listening at http://%s:%s", host, port) 
   })
+
